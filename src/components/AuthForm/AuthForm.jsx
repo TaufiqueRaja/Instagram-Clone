9bed231e0aed18e7680377f332899c852ee36dbd
Taufique Raja <80124042+TaufiqueRaja@users.noreply.github.com>
2024-02-06 14:29:14 +0530

    update

--- src/components/AuthForm/AuthForm.jsx
+++ src/components/AuthForm/AuthForm.jsx
@@ -27,7 +27,7 @@ const AuthForm = () => {
             <Box flex={2} h={"1px"} bg={"gray.400"}/>
         </Flex>
 
-           <GoogleAuth />
+          <GoogleAuth prefix={isLogin ? "Log in" : "Sign up"} />
 
          </VStack>
     </Box>
